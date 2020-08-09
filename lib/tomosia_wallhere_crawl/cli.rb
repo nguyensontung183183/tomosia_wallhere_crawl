61b4cbe23ab97917ea9ad090b04e788b22a9f597
tung <nguyensontung18183>
2020-08-09 15:49:54 +0700

    add

--- lib/tomosia_wallhere_crawl/cli.rb
+++ lib/tomosia_wallhere_crawl/cli.rb
@@ -1,9 +1,13 @@
 require 'thor'
+require_relative '../tomosia_wallhere_crawl'
+
 module TomosiaWallhereCrawl 
   class Cli < Thor
-    desc "sayHello NAME", "Say hello to NAME"
-    def sayHello name
-      puts "Hello #{name}"
+    desc "crawl KEYWORD", "enter KEYWORD to search"
+    option :destination
+    option :max
+    def crawldata(keyword)
+      TomosiaIcon8Crawl::CrawlIcon8.crawldata(keyword, options[:destination], options[:max].to_i)
     end
   end
 end
